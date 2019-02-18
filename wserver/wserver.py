e1a0d03cca6066beb79eb924f190d5e11e40b43c
Maxime Daniel <root@maxux.net>
2019-02-19 00:57:37 +0100

    Improve wserver script

--- wserver/wserver.py
+++ wserver/wserver.py
@@ -1,13 +1,22 @@
 import asyncio
 import websockets
 import json
+import binascii
 from socket import *
 
-def sendeth(source, destination, etype, payload, interface="bond0"):
+srcmac = "b8:27:eb:51:1e:b3"
+dstmac = "a2:43:42:42:42:01"
+listen = "0.0.0.0"
+wsport = 8765
+
+def mactobin(macaddr):
+    return binascii.unhexlify(macaddr.replace(":", ""))
+
+def sendeth(destination, source, etype, payload, interface="eth0"):
     s = socket(AF_PACKET, SOCK_RAW)
     s.bind((interface, 0))
 
-    s.send(source + destination + etype + payload)
+    s.send(mactobin(destination) + mactobin(source) + etype + payload)
 
 async def colorpick(websocket, path):
     while True:
@@ -17,12 +26,13 @@ async def colorpick(websocket, path):
         print(color)
 
         bcolor = bytes([color['r'], color['g'], color['b']])
-        sendeth(b"\xa2\x43\x42\x42\x42\x01", b"\x28\xf1\x0e\x01\x6b\x84", b"\x88\xb6", bcolor)
+        sendeth(dstmac, srcmac, b"\x88\xb6", bcolor)
 
         await websocket.send(json.dumps({"status": "success"}))
 
-start_server = websockets.serve(colorpick, '0.0.0.0', 8765)
+start_server = websockets.serve(colorpick, listen, wsport)
 
+print("[+] listening on ws://%s:%d" % (listen, wsport))
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
 
